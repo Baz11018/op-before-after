6521e4930c15f8375608d6c4dc07cfb8259492f1
Baz11018 <optrading.dubai@gmail.com>
2026-09-24 23:03:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='op-before-after-v1-2',PREFIX='op-before-after-';
+const CACHE='op-before-after-v1-3',PREFIX='op-before-after-';
 const ASSETS=['./','./index.html','./manifest.json','./before-after-icon-192.png','./before-after-icon-512.png','./apple-touch-icon-before-after.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
